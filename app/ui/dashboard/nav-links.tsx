929b4f382e06d292fab7b2aa0afd034b797dfd4a
Matti Luukkainen <mluukkai@iki.fi>
2024-09-03 19:23:51 +0300

    tweak

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -10,7 +10,6 @@ import Link from 'next/link';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
 
-// Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
